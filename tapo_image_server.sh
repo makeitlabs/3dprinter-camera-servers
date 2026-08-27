09db5d62628032ca8484c43aca7170d96aab1127
robojay <jay@reactivetechnologies.com>
2026-08-27 15:04:30 -0400

    Show example crontab entry

--- tapo_image_server.sh
+++ tapo_image_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# add entry into crontab to restart if not running
+# * * * * * pgrep -x "tapo_image_server.s" > /dev/null || /home/jayfr/3dprinter-camera-servers/tapo_image_server.sh > /dev/null 2>&1
+
 cd /home/jayfr/image_server_test
 
 # change the paths appropriately...
