ce999118c1762648180542712b88855683cdaa12
robojay <jay@reactivetechnologies.com>
2026-08-27 15:28:29 -0400

    Correct path

--- tapo_image_server.sh
+++ tapo_image_server.sh
@@ -3,7 +3,7 @@
 # add entry into crontab to restart if not running
 # * * * * * pgrep -x "tapo_image_server.s" > /dev/null || /home/jayfr/3dprinter-camera-servers/tapo_image_server.sh > /dev/null 2>&1
 
-cd /home/jayfr/image_server_test
+cd /home/jayfr/3dprinter-camera-servers
 
 # change the paths appropriately...
-sudo /usr/bin/python3 /home/jayfr/image_server_test/tapo_image_server.py
+sudo /usr/bin/python3 /home/jayfr/3dprinter-camera-servers/tapo_image_server.py
