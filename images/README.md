02390b686ab3d495503f05bdf9d8470e01820783
robojay <jay@reactivetechnologies.com>
2026-08-27 14:28:49 -0400

    Placeholder directory for images, .sh to start server

---
+++ images/README.md
@@ -0,0 +1,1 @@
+This directory will hold images to be served.
---
+++ tapo_image_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/jayfr/image_server_test
+
+# change the paths appropriately...
+sudo /usr/bin/python3 /home/jayfr/image_server_test/tapo_image_server.py
